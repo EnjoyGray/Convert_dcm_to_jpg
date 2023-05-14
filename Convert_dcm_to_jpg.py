1360713a2754e022f868c1e7a3849838d3186ae3
EnjoyGray <vanya.konrad@gmail.com>
2023-05-14 20:56:00 +0200

    added a function to select the folder for saving the source files

--- Convert_dcm_to_jpg.py
+++ Convert_dcm_to_jpg.py
@@ -2,15 +2,15 @@ import os
 import pydicom
 from PIL import Image
 import numpy as np
+from tkinter import Tk
+from tkinter import filedialog
 
 
-def convert_dcm_to_jpeg(dcm_folder,
-                        output_folder):
+def convert_dcm_to_jpeg(dcm_folder, output_folder):
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
-    dcm_files = [f for f in os.listdir(dcm_folder)
-                 if f.endswith('.dcm')]
+    dcm_files = [f for f in os.listdir(dcm_folder) if f.endswith('.dcm')]
 
     for dcm_file in dcm_files:
         dcm_path = os.path.join(dcm_folder, dcm_file)
@@ -20,8 +20,7 @@ def convert_dcm_to_jpeg(dcm_folder,
         # Normalize pixel values to an 8-bit range
         image = (image / np.max(image) * 255).astype(np.uint8)
 
-        output_path = os.path.join(output_folder,
-                                   f"{os.path.splitext(dcm_file)[0]}.jpeg")
+        output_path = os.path.join(output_folder, f"{os.path.splitext(dcm_file)[0]}.jpeg")
 
         # Save the image as JPEG
         image = Image.fromarray(image)
@@ -30,11 +29,17 @@ def convert_dcm_to_jpeg(dcm_folder,
         print(f"File successfully converted to JPEG: {output_path}")
 
 
+def choose_output_folder():
+    root = Tk()
+    root.withdraw()
+    folder_path = filedialog.askdirectory()
+    root.destroy()
+    return folder_path
+
+
 # Example usage of the conversion function
-dcm_folder_path = "path_to_dcm_folder"
-output_folder_path = "/jpeg_files"
+dcm_folder_path = r"files\V_bolt"
 
 
-if __name__ == "main":
-    convert_dcm_to_jpeg(dcm_folder_path,
-                        output_folder_path)
+if __name__ == "__main__":
+    convert_dcm_to_jpeg(dcm_folder_path, choose_output_folder())
