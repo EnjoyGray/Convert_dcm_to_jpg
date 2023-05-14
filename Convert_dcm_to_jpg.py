31d1fc8e73386d421936292094fd316a0855f78d
EnjoyGray <vanya.konrad@gmail.com>
2023-05-14 12:11:55 +0200

    the code is now more readable code

--- Convert_dcm_to_jpg.py
+++ Convert_dcm_to_jpg.py
@@ -3,11 +3,14 @@ import pydicom
 from PIL import Image
 import numpy as np
 
-def convert_dcm_to_jpeg(dcm_folder, output_folder):
+
+def convert_dcm_to_jpeg(dcm_folder,
+                        output_folder):
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
-    dcm_files = [f for f in os.listdir(dcm_folder) if f.endswith('.dcm')]
+    dcm_files = [f for f in os.listdir(dcm_folder)
+                 if f.endswith('.dcm')]
 
     for dcm_file in dcm_files:
         dcm_path = os.path.join(dcm_folder, dcm_file)
@@ -17,7 +20,8 @@ def convert_dcm_to_jpeg(dcm_folder, output_folder):
         # Normalize pixel values to an 8-bit range
         image = (image / np.max(image) * 255).astype(np.uint8)
 
-        output_path = os.path.join(output_folder, f"{os.path.splitext(dcm_file)[0]}.jpeg")
+        output_path = os.path.join(output_folder,
+                                   f"{os.path.splitext(dcm_file)[0]}.jpeg")
 
         # Save the image as JPEG
         image = Image.fromarray(image)
@@ -30,4 +34,7 @@ def convert_dcm_to_jpeg(dcm_folder, output_folder):
 dcm_folder_path = "path_to_dcm_folder"
 output_folder_path = "/jpeg_files"
 
-convert_dcm_to_jpeg(dcm_folder_path, output_folder_path)
+
+if __name__ == "main":
+    convert_dcm_to_jpeg(dcm_folder_path,
+                        output_folder_path)
